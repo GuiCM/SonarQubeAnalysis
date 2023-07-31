b627147d69d9c5b3a6560ec3731ebd4e09a75efa
guilherme.milak <guilherme.cmilak@gmail.com>
2023-07-30 23:16:34 -0300

    Add files

---
+++ init.sql
@@ -0,0 +1,9 @@
+﻿CREATE USER sonar_user WITH ENCRYPTED PASSWORD 'sonar_admin';
+
+CREATE DATABASE sonar_platform_service;
+
+GRANT ALL PRIVILEGES ON DATABASE sonar_platform_services TO sonar_user;
+
+\c sonar_platform_services
+
+GRANT USAGE, CREATE ON SCHEMA public TO sonar_user;
